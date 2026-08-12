619cba21685eeda07aea42cb04d8992e6a9da632
Wandy Reynand Lim <wandylims20@gmail.com>
2026-08-12 11:22:16 +0700

    feat: Added interface on the store

---
+++ store/store.go
@@ -0,0 +1,19 @@
+package store
+
+import "ticket-booking/models"
+
+type FlightStore interface {
+	GetAllFlights() ([]models.Flight, error)
+	GetAllFlightByID(id string) (models.Flight, error)
+}
+
+type SeatStore interface {
+	GetSeatByFlightID(flightID string) ([]models.Seat, error)
+	GetSeatByID(id string) (models.Seat, error)
+}
+
+type BookingStore interface {
+	CreateBooking(booking models.Booking, seats []models.BookingSeat) error
+	GetBookingByID(id string) (models.Booking, error)
+	CancelBooking(id string) error
+}
